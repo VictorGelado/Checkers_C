059bc9773aa1450283eb7e6f47c689e794a523e5
VictorGelado <VictorGelado>
2024-06-26 11:22:42 -0300

    capture pices

--- projeto_ip.c
+++ projeto_ip.c
@@ -59,7 +59,39 @@ void displayCheckers(int ch[CHECKERSLEN][CHECKERSLEN]) {
     printf("\n--------------------------------------------------\n\n");
 }
 
+int capturePiece(int cp, int jp, int cp2, int jp2, int ch[CHECKERSLEN][CHECKERSLEN]) {
+    // Verifica se alguma peca foi comida e retira ela.
+    
+    int cpF = cp - cp2;
+    int jpF = jp - jp2;
+    
+    if (cpF < 0) cpF *= -1;
+    if (jpF < 0) jpF *= -1;
+    
+    if (cpF == 2 && jpF == 2) {
+        
+        if (cp > cp2 && jp > jp2) {
+            ch[cp - 1][jp - 1] = 0;
+        } else if (cp > cp2 && jp < jp2) {
+            ch[cp - 1][jp + 1] = 0;
+        } else if (cp < cp2 && jp < jp2) {
+            ch[cp + 1][jp + 1] = 0;
+        } else {
+            ch[cp + 1][jp - 1] = 0;
+        }
+        
+        return 1; 
+    }
+    
+    return 0; 
+    
+    
+    
+}
+
 int checkCaptures(int cp, int jp, int ch[CHECKERSLEN][CHECKERSLEN]){
+    // Verifica se existe como o jogador fazer mais uma jogada
+    
     // c > 0
     int idx1[2] = {cp-1, jp-1};  // j > 0
     int idx2[2] = {cp-1, jp+1}; // j < 7
@@ -68,6 +100,9 @@ int checkCaptures(int cp, int jp, int ch[CHECKERSLEN][CHECKERSLEN]){
     int idx3[2] = {cp+1, jp+1}; // j < 7
     int idx4[2] = {cp+1, jp-1}; // j > 0
     
+    //Verificar se existe uma peca ocupando a posicao cp+2 e jp+2
+    
+    //cp+1 == 0
     // Verificando se a posicao e menor que o primeiro indice (0)
     if (jp - 1 >= 0 && cp - 1 >= 0) {
         if ((ch[idx1[0]][idx1[1]] != ch[cp][jp] && ch[idx1[0]][idx1[1]] != 0)) {
@@ -105,14 +140,26 @@ int verifyValidMove(int cp, int jp, int cp2, int jp2, int ch[CHECKERSLEN][CHECKE
     int cpF = cp - cp2;
     int jpF = jp - jp2;
     
-    
-    
     if (cpF < 0) cpF *= -1;
     if (jpF < 0) jpF *= -1;
     
-    int previousSquareC = cp-1;
-    int previousSquareJ = jp-1;
-
+    int previousSquareC;
+    int previousSquareJ;
+    
+    if (cp > cp2 && jp > jp2) {
+        previousSquareC = cp-1;
+        previousSquareJ = jp-1;
+    } else if (cp > cp2 && jp < jp2) {
+        previousSquareC = cp-1;
+        previousSquareJ = jp+1;
+    } else if (cp < cp2 && jp < jp2) {
+        previousSquareC = cp+1;
+        previousSquareJ = jp+1;
+    } else {
+        previousSquareC = cp+1;
+        previousSquareJ = jp-1;
+    }
+    
     if (ch[cp][jp] == 2) {
         if (
             (cpF == 1 && jpF == 1 && ch[cp2][jp2] == 0 && cp > cp2) ||
@@ -147,14 +194,14 @@ void makeMove(int ch[CHECKERSLEN][CHECKERSLEN], int *whoseTurn) {
         cp--; jp--;
     } while (!(ch[cp][jp] == *whoseTurn));
     
-    int teste;
+    int capturePieceBool;
     
     do {
         do {
             // Posicao de destino da peca.
             
             displayCheckers(ch);
-            printf("Digite a linha e coluna para onde você quer mover a peça (separado por espaço): ");
+            printf("%c, digite a linha e coluna para onde você quer mover a peça (separado por espaço): ", *whoseTurn == 1? 'X': 'O');
             scanf("%d %d", &cp2, &jp2);
             
             cp2--; jp2--;
@@ -167,9 +214,11 @@ void makeMove(int ch[CHECKERSLEN][CHECKERSLEN], int *whoseTurn) {
         displayCheckers(ch);
         
         // Troca os indices da posicao da peca, pois, ela foi movida de cp para cp2
+        capturePieceBool = capturePiece(cp, jp, cp2, jp2, ch);
+        
         cp = cp2;
         jp = jp2;
-    } while (checkCaptures(cp, jp, ch));
+    } while (checkCaptures(cp, jp, ch) && capturePieceBool);
     
     
     *whoseTurn = *whoseTurn == 1? 2: 1; 
@@ -194,6 +243,9 @@ void fillCheckers(int ch[CHECKERSLEN][CHECKERSLEN]) {
             ch[c][j] = c > 4? 2: 1;
         }
     }
+    
+    ch[4][2] = 2;
+    ch[5][1] = 0;
 
     /*for (c = 5; c < CHECKERSLEN; c++) {
         if (c%2==0) j = 1;   
